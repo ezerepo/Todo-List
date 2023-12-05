08fab00097c0ca98840838dfbe67c3480d6b3091
ezescrypto <116566171+ezescrypto@users.noreply.github.com>
2023-12-05 12:08:25 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [todos, setTodos] = useState([]);
+    const [mademade, setMademade] = useState([]);
+    
+    
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem('todos'));
+        const storedMademade = JSON.parse(localStorage.getItem('mademade'));
+        if (storedTodos.length > 0) {
+            setTodos(storedTodos)   
+        }
+        if (storedTodos.length > 0 && storedMademade > -2) {
+            setMademade(storedMademade)
+        }
+        if (storedTodos.length > 0 && storedMademade < -2) {
+            setMademade(-1)
+        }
+    }, [])
+    
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+        localStorage.setItem('mademade', JSON.stringify(mademade))
+    },   [todos])
+
+   
+    function addTodo(text) {
+        setTodos([...todos,text]);
+    }
+
+    function removeTodo(index) {
+        setTodos(todos.filter((todo,i) => i !== index));
+    }
+
+    function editTodo(index, text) {
+        const newTodos = [...todos];
+        newTodos[index] = text;
+        setTodos(newTodos)
+    }
+
+    function clearTodo() {
+        setTodos(([]))
+    }
+
+    return (
+    <>
+            <h1>Todo App</h1>
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    setMademade(mademade+1);
+                    addTodo(event.target.elements.todo.value)
+                    event.target.elements.todo.value = ''
+                } }>
+                <label>Enter your errand:
+                    <input type="text" name="todo"/>
+                </label>
+                <button type="submit">Add to To Do List </button>
+            </form>
+            <ul>{todos.map((todo, index) => (  
+                <li key={index}>
+                    <input type="text" value={todo} onChange={(event) => editTodo(index, event.target.value)} />
+                    <button
+                        onClick={() => {
+                            removeTodo(index)
+                            setMademade(mademade-1);
+                        }
+                        }> Remove Item </button>
+                    
+                    
+                    
+                </li>
+                ))}
+                <div>
+                    {mademade > 0 && <button
+                        onClick={() => {
+                            clearTodo()
+                            setMademade(-1)
+                        }
+                        }> Clear All Items </button>
+                    }
+                    </div>
+
+            </ul>
+         
+    </>
   );
-}
+} 
 
 export default App;
---
+++ src/component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Test = () => {
+return (
+    <button onClick={() => clearTodo()}> Clear All Items </button>
+
+    )
+}
